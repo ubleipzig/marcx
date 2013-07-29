21ba34a728c5463851b72c43c24677bd6744ba9d
Martin Czygan <martin.czygan@gmail.com>
2013-07-29 15:07:27 +0200

    added trove classifier, long description

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 
+""" 
+marcx consists mainly of a `pymarc.Record` subclass called `FatRecord`,
+that implements a few extra methods, which should ease MARC21 library
+metadata manipulations.
+
+Documentation and examples can be found under: https://github.com/miku/marcx
+"""
+
 from distutils.core import setup
+
+classifiers = """
+Development Status :: 4 - Beta
+Intended Audience :: Education
+Intended Audience :: Developers
+Intended Audience :: Information Technology
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Topic :: Text Processing :: General
+"""
 
 setup(name='marcx',
       version='0.1.3',
-      description='Marc Record Utility',
+      description='MARC21 record manipulation library based on pymarc',
+      long_description=__doc__,
+      classifiers=filter(None, classifiers.split('\n')),
       author='Martin Czygan',
       author_email='martin.czygan@gmail.com',
       url='https://github.com/miku/marcx',
