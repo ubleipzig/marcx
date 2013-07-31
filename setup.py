2acd69fdc36d26229e4567b939ccc66ab80ef6a1
Martin Czygan <martin.czygan@gmail.com>
2013-07-31 15:08:09 +0200

    version bump

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ Topic :: Text Processing :: General
 """
 
 setup(name='marcx',
-      version='0.1.4',
+      version='0.1.5',
       description='MARC21 record manipulation library based on pymarc',
       long_description=__doc__,
       classifiers=filter(None, classifiers.split('\n')),
