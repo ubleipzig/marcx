431c09049287b25bcd8d5056f9259c2caecfe554
Martin Czygan <martin.czygan@gmail.com>
2013-08-01 14:15:17 +0200

    setup.py wording

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 """ 
 marcx consists mainly of a `pymarc.Record` subclass called `FatRecord`,
-that implements a few extra methods, which should ease MARC21 library
+that implements a few extra methods, which should ease MARC library
 metadata manipulations.
 
 Documentation and examples can be found under: https://github.com/miku/marcx
@@ -22,7 +22,7 @@ Topic :: Text Processing :: General
 
 setup(name='marcx',
       version='0.1.6',
-      description='MARC21 record manipulation library based on pymarc',
+      description='MARC record manipulation library based on pymarc',
       long_description=__doc__,
       classifiers=filter(None, classifiers.split('\n')),
       author='Martin Czygan',
