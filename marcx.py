896fc667b1b67e12ca676116996100db56df6a05
Martin Czygan <martin.czygan@gmail.com>
2013-09-09 20:41:09 +0200

    version bump

--- marcx.py
+++ marcx.py
@@ -15,7 +15,7 @@ import itertools
 import pyisbn
 import re
 
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 __all__ = [
     'FatRecord',
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ Topic :: Text Processing :: General
 """
 
 setup(name='marcx',
-      version='0.1.7',
+      version='0.1.8',
       description='MARC record manipulation library based on pymarc',
       long_description=__doc__,
       classifiers=filter(None, classifiers.split('\n')),
