a50a93ef318fa6672d25eab356fd342791a80b6b
Martin Czygan <martin.czygan@gmail.com>
2014-07-16 17:09:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ marcx consists mainly of a `pymarc.Record` subclass called `FatRecord`,
 that implements a few extra methods, which should ease MARC library
 metadata manipulations.
 
-Documentation and examples can be found under: https://github.com/miku/marcx
+Documentation and examples can be found under: https://github.com/ubleipzig/marcx
 """
 
 from setuptools import setup
@@ -27,6 +27,6 @@ setup(name='marcx',
       classifiers=filter(None, classifiers.split('\n')),
       author='Martin Czygan',
       author_email='martin.czygan@gmail.com',
-      url='https://github.com/miku/marcx',
+      url='https://github.com/ubleipzig/marcx',
       py_modules=['marcx'],
       install_requires=['pymarc>=2.0', 'jsonpath-rw==1.3.0', 'ply==3.4'])
