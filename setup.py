094fca0f54144f28fb3aa39cf7b35e1cd3d16c8f
Martin Czygan <martin.czygan@gmail.com>
2017-07-17 15:00:20 +0200

    v0.2.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ Topic :: Text Processing :: General
 """
 
 setup(name='marcx',
-      version='0.2.0',
+      version='0.2.1',
       description='MARC record manipulation library based on pymarc',
       long_description=__doc__,
       classifiers=filter(None, classifiers.split('\n')),
